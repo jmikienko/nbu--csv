d992ea842a413d4bdeeaa1836f8b3e3c65e1d368
jmikienko <73592168+jmikienko@users.noreply.github.com>
2020-10-29 16:31:28 +0100

    Update nbuimport.py

--- nbuimport.py
+++ nbuimport.py
@@ -3,6 +3,47 @@
 # open netbackup config file - output of bppllist -L -allpolicies
 streamPolicies = open("bppllist.txt")
 
+nbu_retention_levels = {
+    "0" : "1 week",
+    "1": "2 weeks",
+    "2": "3 weeks",
+    "3" : "1 month",
+    "4": "2 months",
+    "5": "3 months",
+    "6": "6 months",
+    "7": "9 months",
+    "8" : "1 year",
+    "9": "infinite",
+     }
+
+nbu_policy_type = {
+"0" : "Standard (UNIX and Linux clients)",
+"1" : "Proxy",
+"4" : "Oracle",
+"6" : "Informix-On-BAR",
+"7" : "Sybase",
+"8" : "MS-SharePoint portal server",
+"11" : "DataTools-SQL-BackTrack",
+"13" : "MS- Windows",
+"15" : "MS-SQL-Server",
+"16" : "MS-Exchange-Server",
+"17" : "SAP",
+"18" : "DB2",
+"19" : "NDMP",
+"20" : "FlashBackup",
+"21" : "Splitmirror",
+"25" : "Lotus Notes",
+"29" : "FlashBackup-Windows",
+"35" : "NBU-Catalog",
+"36" : "Generic",
+"38" : "PureDisk export",
+"39" : "Enterprise_Vault",
+"40" : "VMware",
+"41" : "Hyper-V",
+"44" : "BigData"
+}
+
+
 # for each policy generate line
 while 1 == 1:
     policy = streamPolicies.readline()
@@ -16,14 +57,20 @@ while 1 == 1:
         print ("POLICY: ", tab_params)
 
     if tab_params[0] == "INFO":
-        print ("INFO: ", tab_params)
+        print ("INFO: This policy is ", nbu_policy_type[tab_params[1]], "policy")
 
     if tab_params[0] == "SCHED":
         print ("SCHED: ", tab_params)
         if tab_params[2] == "0":
             print("this is full schedule")
-        print("it runs every", tab_params[4], "seconds")
+        if tab_params[2] == "1":
+            print("this is differential schedule")
+        if tab_params[2] == "2":
+            print("this is cumulative incrmental schedule")
+
+        print("it runs every", int(tab_params[4])/3600, "hours")
         print("its retention level is", tab_params[5])
+        print("it keeps data for", nbu_retention_levels[tab_params[5]])
 
     if tab_params[0] == "SCHEDWIN":
         print ("WINDOW: ", tab_params)
